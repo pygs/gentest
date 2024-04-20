d9fb83fcba5e1955e7399901b2686cfa6dabc15f
pygs <pygs1500@gmail.com>
2024-04-21 00:35:27 +0200

    created adding questions and answers

--- main.py
+++ main.py
@@ -66,6 +66,17 @@ def get_subject_id(conn, subject_name):
         messagebox.showerror("Błąd", "Błąd podczas pobierania ID przedmiotu:\n{}".format(e))
         return None
     
+def get_topic_id(conn, topic_name):
+    try:
+        cursor = conn.cursor()
+        cursor.execute("SELECT id FROM topics WHERE name = %s", (topic_name,))
+        subject_id = cursor.fetchone()[0]
+        cursor.close()
+        return subject_id
+    except psycopg2.Error as e:
+        messagebox.showerror("Błąd", "Błąd podczas pobierania ID tematu:\n{}".format(e))
+        return None
+    
 
 
 def open_main_window(conn):
@@ -87,9 +98,8 @@ def open_main_window(conn):
     subject_plus_button = tk.Button(main_window, text="+", command=lambda: open_add_subject_window(conn, subject_value, subject_box))
     subject_plus_button.grid(row=0, column=2)
     
-    subject_value.trace_add("write", lambda name, index, mode, sv=subject_value: on_combobox_select(conn, sv.get()))
-    subject_value.trace_add("write", lambda *args: refresh_combobox(conn, topic_box, subject_value.get()))
-    subject_id = get_subject_id(conn, subject_value.get())
+    subject_value.trace_add("write", lambda name, index, mode, sv=subject_value: on_subjectbox_select(conn, sv.get()))
+    subject_value.trace_add("write", lambda *args: refresh_topicbox(conn, topic_box, subject_value.get()))
     topics = get_topics(conn, subject_value.get())
     
     
@@ -106,9 +116,16 @@ def open_main_window(conn):
     topic_plus_button = tk.Button(main_window, text="+", command=lambda: open_add_topic_window(conn, topic_value, topic_box, subject_value.get()))
     topic_plus_button.grid(row=1, column=2)
 
+    topic_value.trace_add("write", lambda name, index, mode, tv=topic_value: on_topicbox_select(conn, tv.get()))
+
+    question_label = tk.Label(main_window, text="Dodaj pytanie: ")
+    question_label.grid(row=2, column=0)
+    question_plus_button = tk.Button(main_window, text="+", command=lambda: open_add_question_window(conn, topic_value.get()))
+    question_plus_button.grid(row=2, column=1)
+
     main_window.mainloop()
 
-def refresh_combobox(conn, combo_box, current_subject):
+def refresh_topicbox(conn, combo_box, current_subject):
     # Usunięcie istniejących elementów z comboboxa
     combo_box['menu'].delete(0, 'end')
 
@@ -120,13 +137,21 @@ def refresh_combobox(conn, combo_box, current_subject):
     for topic in topics:
         combo_box['menu'].add_command(label=topic, command=lambda value=topic: topic_value.set(value))
 
-def on_combobox_select(conn, selected_subject):
+def on_subjectbox_select(conn, selected_subject):
     subject_id = get_subject_id(conn, selected_subject)
     if subject_id is not None:
         print("Wybrano przedmiot o ID:", subject_id)
         return subject_id
     else:
         print("Nie można pobrać ID wybranego przedmiotu.")
+
+def on_topicbox_select(conn, selected_topic):
+    topic_id = get_topic_id(conn, selected_topic)
+    if topic_id is not None:
+        print("Wybrano temat o ID:", topic_id)
+        return topic_id
+    else:
+        print("Nie można pobrać ID wybranego tematu.")
 
 def open_add_subject_window(conn, subject_value, subject_box):
     add_window = tk.Toplevel()
@@ -152,6 +177,35 @@ def open_add_topic_window(conn, topic_value, topic_box, current_subject):
     add_button = tk.Button(add_window, text="Dodaj", command=lambda: add_topic(conn, topic_name_entry.get(), topic_value, topic_box, add_window, current_subject))
     add_button.pack()
 
+def open_add_question_window(conn, current_topic):
+    add_window = tk.Toplevel()
+    add_window.title("Dodaj pytanie")
+    add_window.geometry("300x200")
+
+    tk.Label(add_window, text="Pytanie:").grid(row=0, column=0)
+    question_name_entry = tk.Entry(add_window)
+    question_name_entry.grid(row=0, column=1)
+
+    tk.Label(add_window, text="Odpowiedź poprawna:").grid(row=1, column=0)
+    answer_1_entry = tk.Entry(add_window)
+    answer_1_entry.grid(row=1, column=1)
+
+    tk.Label(add_window, text="Odpowiedź 1:").grid(row=2, column=0)
+    answer_2_entry = tk.Entry(add_window)
+    answer_2_entry.grid(row=2, column=1)
+
+    tk.Label(add_window, text="Odpowiedź 2:").grid(row=3, column=0)
+    answer_3_entry = tk.Entry(add_window)
+    answer_3_entry.grid(row=3, column=1)
+
+    tk.Label(add_window, text="Odpowiedź 3:").grid(row=4, column=0)
+    answer_4_entry = tk.Entry(add_window)
+    answer_4_entry.grid(row=4, column=1)
+
+
+    add_button = tk.Button(add_window, text="Dodaj", command=lambda: add_question(conn, question_name_entry.get(), answer_1_entry.get(), answer_2_entry.get(), answer_3_entry.get(), answer_4_entry.get(), add_window, current_topic))
+    add_button.grid(row=5, column=0)
+
 def add_subject(conn, subject_name, subject_value, subject_box, add_window):
     if not subject_name:
         messagebox.showerror("Błąd", "Nazwa przedmiotu nie może być pusta.")
@@ -176,7 +230,7 @@ def add_subject(conn, subject_name, subject_value, subject_box, add_window):
 
 def add_topic(conn, topic_name, topic_value, topic_box, add_window, current_subject):
     if not topic_name:
-        messagebox.showerror("Błąd", "Nazwa przedmiotu nie może być pusta.")
+        messagebox.showerror("Błąd", "Nazwa tematu nie może być pusta.")
         return
     
     try:
@@ -196,6 +250,27 @@ def add_topic(conn, topic_name, topic_value, topic_box, add_window, current_subj
 
     except psycopg2.Error as e:
         messagebox.showerror("Błąd", "Błąd podczas dodawania tematu:\n{}".format(e))
+
+def add_question(conn, question_name, a1, a2, a3, a4, add_window, current_topic):
+    if not question_name:
+        messagebox.showerror("Błąd", "Nazwa pytania nie może być pusta.")
+        return
+    if not a1 or a2 or a3 or a4:
+        messagebox.showerror("Błąd", "Odpowiedź nie może być pusta.")
+        return
+    
+    try:
+        cursor = conn.cursor()
+        topic_id = get_topic_id(conn, current_topic)
+        cursor.execute("INSERT INTO qa (topic_id, question, answer_1, answer_2, answer_3, answer_4) VALUES (%s, %s, %s, %s, %s, %s)", (topic_id, question_name, a1, a2, a3, a4))
+        conn.commit()
+        cursor.close()
+        messagebox.showinfo("Sukces", "Pytanie dodane pomyślnie!")
+
+        add_window.destroy()
+
+    except psycopg2.Error as e:
+        messagebox.showerror("Błąd", "Błąd podczas dodawania pytania:\n{}".format(e))
 
 def get_subjects(conn):
     try:
